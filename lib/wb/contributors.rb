fd8c4d4bb0948eb6ea5ea75f05845ccc86494e12
Tim Smith <tsmith@chef.io>
2017-01-20 20:27:47 -0800

    Dump additional contribution information to Dynamo
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/wb/contributors.rb
+++ lib/wb/contributors.rb
@@ -5,6 +5,7 @@ module WelcomeBot
 
     string_attr     :username, hash_key: true
     datetime_attr   :first_contribution
+    string_attr     :pr_url
 
     #validates_presence_of :username
   end
--- lib/wb/dynamodb.rb
+++ lib/wb/dynamodb.rb
@@ -8,21 +8,21 @@ module WelcomeBot
       Aws::DynamoDB::Client.new(
         region: WelcomeBot::Config.aws_region,
         access_key_id: WelcomeBot::Config.aws_access_key_id,
-        secret_access_key: WelcomeBot::Config.aws_secret_access_key,
+        secret_access_key: WelcomeBot::Config.aws_secret_access_key
       )
     end
 
     def table_exists?
-      @dyndb_client.list_tables.table_names.include?('WelcomeBot_Contributors')
+      @dyndb_client.list_tables.table_names.include?("WelcomeBot_Contributors")
     end
 
     def run_migration
       puts "Setting up DynamoDB table. This make take a bit..."
-      migration = Aws::Record::TableMigration.new(WelcomeBot::Contributors, opts = { client: @dyndb_client})
+      migration = Aws::Record::TableMigration.new(WelcomeBot::Contributors, opts = { client: @dyndb_client })
       migration.create!(
         provisioned_throughput: {
           read_capacity_units: 1,
-          write_capacity_units: 1
+          write_capacity_units: 1,
         }
       )
       migration.wait_until_available
